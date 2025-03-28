043c94a3ee992db18d487932cd3b06c12f3375a5
KAYO ALMEIDA TRINDADE <202404269027@alunos.unifanor.edu.br>
2025-03-28 10:50:50 -0300

    kayo

---
+++ operadoresComparacao.py
@@ -0,0 +1,23 @@
+
+x = 10
+y = 10
+
+# Comparacao de igualdade
+print (x == y)
+
+# Diferença 
+print(x != y)
+
+# Superioridade (>)
+print(x > y)
+
+# Inferioridade (<)
+
+print(x < y)
+
+# Maior ou igual (>=)
+
+print (x > y)
+# Menor ou igual (<=)
+print (x <= y)
+
