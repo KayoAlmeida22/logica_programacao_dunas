9e9d719d35b4ad55bb039840e5dce650e9544904
KAYO ALMEIDA TRINDADE <202404269027@alunos.unifanor.edu.br>
2025-04-04 10:55:39 -0300

    kayo

---
+++ condicionalComposta.py
@@ -0,0 +1,9 @@
+# x = 5
+# if x == 5:
+#    print ("x é igual a 5")
+
+numeroMatricula = 202404269027
+if numeroMatricula == 202404269027:
+    print("esse é o numero de matricula do kayo")
+else:
+     print("não é o numero de matrícula do kayo")
---
+++ exercicios/desafio01.py
@@ -0,0 +1,11 @@
+#atividade
+
+x=(input("1;"))
+y=(input("3;"))
+
+if x==y:
+    print("x é igual a y")
+elif x>y:
+    print ("x é maior que y")
+else:
+    print ("x é menor que y") 
